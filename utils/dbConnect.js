f33555d75068955b734294c2d0b12656294e2786
ahrenpradhan <ahrenpradhan@gmail.com>
2021-01-02 09:56:09 +0530

    just to check

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -9,6 +9,8 @@ async function dbConnect() {
 	}
 
 	/* connecting to our database */
+	console.log('-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- ');
+	console.log(process.env.MONGODB_URI);
 	const db = await mongoose.connect(process.env.MONGODB_URI, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
